afa509d7a7a03c0370a4499cb9009a38d9e97b82
seth <seth.gainey@navy.mil>
2012-11-20 10:04:45 -0500

    Add in-project repo.

--- src/main/java/gov/va/iehr/hdata/hdr/RootDocumentImpl.java
+++ src/main/java/gov/va/iehr/hdata/hdr/RootDocumentImpl.java
@@ -16,14 +16,17 @@
 package gov.va.iehr.hdata.hdr;
 
 import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
 import org.projecthdata.javahstore.hdr.DocumentMetadata;
 import org.projecthdata.javahstore.hdr.Extension;
 import org.projecthdata.javahstore.hdr.RootDocument;
@@ -32,101 +35,129 @@ import org.projecthdata.javahstore.hdr.SectionDocument;
 
 /**
  * Sample implementation of RootDocument with a single section for allergies
+ *
  * @author marc
  */
 public class RootDocumentImpl implements RootDocument {
+    
+    private String id;
+    Map<String, Extension> extensions;
+    List<Section> rootSections;
+    Date created;
+    Date updated;
+    private static final String TEST_AUTHOR = "Fred Bloggs";
+    private static final String TEST_METADATA = "<DocumentMetaData xmlns='http://projecthdata.org/hdata/schemas/2009/11/metadata'>"
+            + "<PedigreeInfo>"
+            + "<Author>"
+            + TEST_AUTHOR
+            + "</Author>"
+            + "</PedigreeInfo>"
+            + "<DocumentId>xyzzy</DocumentId>"
+            + "<Title>The Title</Title>"
+            + "<RecordDate>"
+            + "<CreatedDateTime>2006-05-04T18:13:51.0Z</CreatedDateTime>"
+            + "</RecordDate>"
+            + "</DocumentMetaData>";
 
-  private String id;
-  Map<String, Extension> extensions;
-  List<Section> rootSections;
-  Date created;
-  Date updated;
-
-  private static final String TEST_AUTHOR = "Fred Bloggs";
-  private static final String TEST_METADATA = "<DocumentMetaData xmlns='http://projecthdata.org/hdata/schemas/2009/11/metadata'>"
-          + "<PedigreeInfo>"
-          + "<Author>"
-          + TEST_AUTHOR
-          + "</Author>"
-          + "</PedigreeInfo>"
-          + "<DocumentId>xyzzy</DocumentId>"
-          + "<Title>The Title</Title>"
-          + "<RecordDate>"
-          + "<CreatedDateTime>2006-05-04T18:13:51.0Z</CreatedDateTime>"
-          + "</RecordDate>"
-          + "</DocumentMetaData>";
-
-  /**
-   * Build a fixed record with a single allergies extension, corresponding
-   * section and with one document in the allergies section
-   * @param recordId
-   */
-  RootDocumentImpl(String recordId) {
-    this.id = recordId;
-    this.created = this.updated = new Date();
-    this.extensions = new HashMap<String, Extension>();
-    Extension allergiesExtension = new AllergiesExtension();
-    this.extensions.put(allergiesExtension.getId(), allergiesExtension);
-    this.rootSections = new ArrayList<Section>();
-    String sectionPath = "allergies";
-    createChildSection(allergiesExtension, sectionPath, "Allergy Section");
-    Section allergiesSection = getChildSection(sectionPath);
-    try {
-      SectionDocument secDoc = allergiesSection.createDocument("text/plain", new ByteArrayInputStream("Hello World !".getBytes()));
-      DocumentMetadata docMeta = new DocumentMetadata(TEST_METADATA);
-      secDoc.setMetadata(docMeta);
-    } catch (Exception ex) {
-      Logger.getLogger(RootDocumentImpl.class.getName()).log(Level.SEVERE, null, ex);
+    /**
+     * @param xmlFilePath
+     */
+    RootDocumentImpl(String patientId) {
+        
+        // SAX parser used because we do not require the xml manipulation of DOM
+        SAXBuilder parser = new SAXBuilder();
+        Document doc = null;
+        try {
+            doc = parser.build(new File("C:\\Projects\\IEHR\\xml\\ccr\\1_Sofia_Weideman.xml"));
+        } catch (JDOMException ex) {
+            Logger.getLogger(RootDocumentImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(RootDocumentImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Element root = doc.getRootElement();
+        Namespace ns = root.getNamespace(); // urn:astm-org:CCR
+        String ccrId = root.getChildText("CCRDocumentObjectID", ns );
+        
+        System.out.println("root = " + root.toString());
+        System.out.println("ns = " + ns.toString());
+//        System.out.println("children = " + root.getChildren);
+        System.out.println("ccrId = " + ccrId);
+                
+        this.id = patientId;
+        this.created = this.updated = new Date();
+        
+        
+        /* hData extensions here
+         * 
+         */
+        this.extensions = new HashMap<String, Extension>();
+        this.rootSections = new ArrayList<Section>();
+//        Extension allergiesExtension = new AllergiesExtension();
+//        this.extensions.put(allergiesExtension.getId(), allergiesExtension);
+//        String sectionPath = "allergies";
+//        createChildSection(allergiesExtension, sectionPath, "Allergy Section");
+//        Section allergiesSection = getChildSection(sectionPath);
+//        try {
+//            SectionDocument secDoc = allergiesSection.createDocument("text/plain", new ByteArrayInputStream("Hello World !".getBytes()));
+//            DocumentMetadata docMeta = new DocumentMetadata(TEST_METADATA);
+//            secDoc.setMetadata(docMeta);
+//        } catch (Exception ex) {
+//            Logger.getLogger(RootDocumentImpl.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
-  }
 
-  @Override
-  public String getId() {
-    return id;
-  }
-
-  @Override
-  public Date getCreated() {
-    return created;
-  }
-
-  @Override
-  public Date getLastModified() {
-    return updated;
-  }
-
-  @Override
-  public Collection<Extension> getExtensions() {
-    return extensions.values();
-  }
-
-  @Override
-  public Extension getExtension(String id) {
-    Extension e = extensions.get(id);
-    if (e==null)
-      throw new IllegalArgumentException("Unknown extension ID: "+id);
-    return e;
-  }
-
-  @Override
-  public Collection<Section> getRootSections() {
-    return rootSections;
-  }
-
-  @Override
-  public final void createChildSection(Extension e, String path, String name) {
-    this.rootSections.add(new GenericChildSection(e, path, name));
-  }
-
-  @Override
-  public final Section getChildSection(String segment) {
-    Section sec = null;
-    for (Section section : rootSections) {
-      if (section.getPath().equals(segment)) {
-        sec = section;
-      }
+    @Override
+    public String getId() {
+        return id;
     }
-    return sec;
-  }
+
+    @Override
+    public Date getCreated() {
+        return created;
+    }
+
+    @Override
+    public Date getLastModified() {
+        return updated;
+    }
+
+    @Override
+    public Collection<Extension> getExtensions() {
+        return extensions.values();
+    }
+
+    @Override
+    public Extension getExtension(String id) {
+        Extension e = extensions.get(id);
+        if (e == null) {
+            throw new IllegalArgumentException("Unknown extension ID: " + id);
+        }
+        return e;
+    }
+
+    @Override
+    public Collection<Section> getRootSections() {
+        return rootSections;
+    }
+
+    @Override
+    public final void createChildSection(Extension e, String path, String name) {
+        this.rootSections.add(new GenericChildSection(e, path, name));
+    }
+
+    @Override
+    public final Section getChildSection(String segment) {
+        Section sec = null;
+        for (Section section : rootSections) {
+            if (section.getPath().equals(segment)) {
+                sec = section;
+            }
+        }
+        return sec;
+    }
+    
+    
+    
+    
 
 }
